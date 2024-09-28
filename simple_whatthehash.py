e141c5c92a74619bf0618fef825b6341ce7bb45f
brady <brady@tkalib.local>
2024-09-27 23:24:57 -0600

    making corrections 1

--- simple_whatthehash.py
+++ simple_whatthehash.py
@@ -16,12 +16,12 @@ class Color:
 def color_reset():
     print(Color.END)
 
-def verify_checksu():
+def verify_checksum():
     print("Enter Checksum Provided by Authorized Distrubutor or Developer...")
     given_checksum = input()
     print(Color.PURPLE + "You entered: " + given_checksum)
     print(Color.GREEN + "Calculated :" + sha256_hash.hexdigest())
-    if given_checksum == sha256.hexdigest():
+    if given_checksum == sha256_hash.hexdigest():
         safe_result = (Color.BOLD + Color.GREEN + "Checksum Verfied! File is OK.")
         print(safe_result)
     else:
@@ -29,4 +29,12 @@ def verify_checksu():
         print(bad_result)
         color_reset()
 
+filename = input("Enter the file name: ")
+sha256_hash = hashlib.sha256()
+with open(filename, "rb") as f:
+    for byte_block in iter(lambda: f.read(4096),b""):
+        sha256_hash.update(byte_block)
+    print(Color.GREEN + "sha256 valule = " + sha256_hash.hexdigest())
+    color_reset()
 
+verify_checksum()
